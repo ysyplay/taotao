c63d419b5954e30be7946bd816e081db30fda9e0
ysyplay <1094510449@qq.com>
2018-01-10 10:22:19 +0800

    pojo提交

--- taotao-common/src/main/java/com/taotao/common/utils/FtpUtil.java
+++ taotao-common/src/main/java/com/taotao/common/utils/FtpUtil.java
@@ -134,14 +134,5 @@ public class FtpUtil {
 		}
 		return result;
 	}
-	
-	public static void main(String[] args) {
-		try {  
-	        FileInputStream in=new FileInputStream(new File("D:\\temp\\image\\gaigeming.jpg"));  
-	        boolean flag = uploadFile("192.168.25.133", 21, "ftpuser", "ftpuser", "/home/ftpuser/www/images","/2015/01/21", "gaigeming.jpg", in);  
-	        System.out.println(flag);  
-	    } catch (FileNotFoundException e) {  
-	        e.printStackTrace();  
-	    }  
-	}
+
 }
